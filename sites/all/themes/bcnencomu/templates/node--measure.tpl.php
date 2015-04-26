13e4867782069f0ff15ce8c2677fa065964db75a
Felip Ladron <fladron@gmail.com>
2015-04-26 21:23:37 +0200

    Added tpl for measure content type

---
+++ sites/all/themes/bcnencomu/templates/node--measure.tpl.php
@@ -0,0 +1,30 @@
+<?php $tag = ($view_mode != 'full')? 'div' : 'article'; ?>
+<<?php print $tag; ?> id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php if (isset($youtube_uri)){ ?> data-youtube-uri="<?php print $youtube_uri; ?>"<?php } ?>>
+  <?php if ($view_mode == 'teaser' || $view_mode == 'highlighted') { ?>
+  <?php /* ----------------- TEASER DISPLAY ----------------- */ ?>
+  
+  <?php } else if ($view_mode == 'slider') { ?>
+  <?php /* ----------------- SLIDER DISPLAY ----------------- */ ?>
+  
+  <?php } else if ($view_mode == 'full') { ?>
+  <?php /* ----------------- FULL DISPLAY ----------------- */ ?>
+  <header>
+    <h1><?php print $title; ?></h1>
+  </header>
+  <div class="content">
+    <?php if (isset($share_links)){ ?>
+      <?php print $share_links; ?>
+    <?php } ?>
+    
+    <?php 
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+     ?>
+  </div>
+  <?php } ?>
+</<?php print $tag; ?>> <!-- /node-->
+<?php if (isset($content['comments'])){ ?>
+<?php print render($content['comments']); ?>
+<?php } 
