3498ad05f1784ca692482aae08e9d6158aa2f264
Felip Ladron <fladron@gmail.com>
2015-04-26 03:07:29 +0200

    Mesures: table measures also open as overlay

--- sites/all/modules/guanyem/mesures_programa/js/mesures.programa.js
+++ sites/all/modules/guanyem/mesures_programa/js/mesures.programa.js
@@ -9,7 +9,7 @@ var programa_config = {
   // ON READY
   $(window).ready(function(){
     // open measure detail as overlay
-    $('.view-measures-list-city, .view-measures-list-district').find('.views-field-title a').each(function(i){
+    $('.view-measures-list-city, .view-measures-list-district, .view-mesures-cercador').find('.views-field-title-field a').each(function(i){
       var href = $(this).attr('href');
       $(this).attr('href', href + '?oasis=1');
       $(this).fancybox(
