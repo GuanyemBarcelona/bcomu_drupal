4f74d49e150aeeb4114074073f58bb7542c995a9
Felip Ladron <fladron@omitsis.com>
2015-05-27 17:35:09 +0200

    Removed List link in Primaries > Council results

--- sites/all/modules/guanyem/primaries_resultats/js/primaries.resultats.js
+++ sites/all/modules/guanyem/primaries_resultats/js/primaries.resultats.js
@@ -10,19 +10,6 @@ var primaries_config = {
   $(window).ready(function(){
   	var $page_results = $('.primaries-resultats');
 
-  	// council
-    if ($page_results.attr('data-voting-type') == 'council'){
-    	// map or list
-    	var $results_panel = $page_results.find('[data-results-type]');
-    	$results_panel.each(function(i){
-    		if ($(this).attr('data-results-type') != 'map'){
-    			$(this).hide();
-    		}
-    		$(this).find('> button.toggle-view').click(function(e){
-    			$results_panel.slideToggle(300);
-    		});
-    	});
-    }
   });
 
 })(jQuery);
--- sites/all/modules/guanyem/primaries_resultats/theme/results_council.tpl.php
+++ sites/all/modules/guanyem/primaries_resultats/theme/results_council.tpl.php
@@ -4,7 +4,6 @@
   </header>
   <div class="content">
     <div data-results-type="map">
-      <button class="toggle-view" data-action="see-list"><i class="fa fa-list"></i> <?php print t("List", array(), array('context' => 'Primaries: resultats')); ?></button>
       <div class="map">
         <section class="voting-winners">
           <?php foreach ($voting['neighbourhoods'] as $key => $neighbourhood) { ?>
@@ -32,58 +31,6 @@
         <?php } ?>
       </div>
     </div>
-    <div data-results-type="list">
-      <button class="toggle-view" data-action="see-map"><i class="fa fa-map-marker"></i> <?php print t("Map", array(), array('context' => 'Primaries: resultats')); ?></button>
-      <?php foreach ($voting['neighbourhoods'] as $key1 => $neighbourhood) { ?>
-      <h2><?php print $neighbourhood['name']; ?></h2>
-      <?php if (count($neighbourhood['answers']) > 0){ ?>
-      <section class="voting-options">
-        <table border="1" cellpadding="1" cellspacing="1" width="100%">
-          <thead>
-            <tr>
-              <th width="5%">#</th>
-              <th width="55%"><?php print t("Name", array(), array('context' => 'Primaries: resultats')); ?></th>
-              <th width="15%"><?php print t("Picture", array(), array('context' => 'Primaries: resultats')); ?></th>
-              <th width="15%"><?php print t("Votes", array(), array('context' => 'Primaries: resultats')); ?></th>
-              <th width="10%"><?php print t("Percentage", array(), array('context' => 'Primaries: resultats')); ?></th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php foreach ($neighbourhood['answers'] as $key2 => $option) { ?>
-            <tr>
-              <td class="position"><?php print($key2 + 1); ?></td>
-              <td class="title"><h2><a href="<?php print $option['urls'][0]['url']; ?>"><?php print $option['text']; ?></a></h2></td>
-              <td class="image">
-                <a href="<?php print $option['urls'][0]['url']; ?>"><img src="<?php print $option['urls'][1]['url']; ?>" /></a>
-              </td>
-              <td class="count"><?php print $option['total_count']; ?></td>
-              <td class="percent"><?php print $option['percent']; ?></td>
-            </tr>
-            <?php } ?>
-          </tbody>
-        </table>
-      </section>
-      <section class="blank-option">
-        <div class="voting-option">
-          <div class="content">
-            <h2><?php print t("Blank votes", array(), array('context' => 'Primaries: resultats')); ?></h2>
-          </div>
-          <dl class="results">
-            <dt class="votes"><?php print t("Votes", array(), array('context' => 'Primaries: resultats')); ?>:</dt>
-            <dd class="votes"><?php print $neighbourhood['blank']['total_count']; ?></dd>
-            <dt class="percent"><?php print t("Percentage", array(), array('context' => 'Primaries: resultats')); ?>:</dt>
-            <dd class="percent"><?php print $neighbourhood['blank']['percent']; ?></dd>
-          </dl>
-        </div>
-      </section>
-      <section class="general">
-        <p class="total-votes"><?php print t("Total votes", array(), array('context' => 'Primaries: resultats')); ?>: <span class="votes"><?php print $neighbourhood['total_votes']; ?></span></p>
-        <a data-action="verify-voting" href="<?php print $neighbourhood['verify_uri']; ?>" rel="external"><?php print t("Verify the results of this voting", array(), array('context' => 'Primaries: resultats')); ?></a>
-      </section>
-      <?php }else{ ?>
-      <p class="empty"><?php print t("No voting was made on this topic", array(), array('context' => 'Primaries: resultats')); ?></p>
-      <?php } ?>
-      <?php } ?>
-    </div>
+    
   </div>
 </article>
