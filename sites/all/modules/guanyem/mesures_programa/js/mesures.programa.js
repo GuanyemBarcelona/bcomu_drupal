7d6b80a21ecdebf18e11a0fbe81c8075fdb22632
Felip Ladron <fladron@gmail.com>
2016-05-14 20:41:46 +0200

    removed unused font awesome icons for the programa

--- sites/all/modules/guanyem/mesures_programa/js/mesures.programa.js
+++ sites/all/modules/guanyem/mesures_programa/js/mesures.programa.js
@@ -20,24 +20,6 @@ var programa_config = {
         }
       );
     });
-
-    // sidebar menu icons
-    var $sidebar_menu = $('.region-sidebar-first ul.menu');
-    if ($sidebar_menu.length){
-      $sidebar_menu.find('li.mid-966 > a').prepend('<i class="fa fa-star-o"></i>');
-      $sidebar_menu.find('li.mid-967 > a').prepend('<i class="fa fa-cogs"></i>');
-      $sidebar_menu.find('li.mid-968 > a').prepend('<i class="fa fa-map-marker"></i>');
-      $sidebar_menu.find('li.mid-969 > a').prepend('<i class="fa fa-tag"></i>');
-      $sidebar_menu.find('li.mid-970 > a').prepend('<i class="fa fa-search"></i>');
-    }
-    // programa navega menu icons
-    var $navega_page_menu = $('article#node-369 .body > ul');
-    if ($navega_page_menu){
-      $navega_page_menu.find('li').eq(0).find('> a').prepend('<i class="fa fa-cogs"></i>');
-      $navega_page_menu.find('li').eq(1).find('> a').prepend('<i class="fa fa-map-marker"></i>');
-      $navega_page_menu.find('li').eq(2).find('> a').prepend('<i class="fa fa-tag"></i>');
-      $navega_page_menu.find('li').eq(3).find('> a').prepend('<i class="fa fa-search"></i>');
-    }
   });
 
 })(jQuery);
