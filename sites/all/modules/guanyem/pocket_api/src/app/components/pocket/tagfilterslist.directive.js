2d5562ef5bf467a6c58978c1aa21a41dff6737c4
Felip Ladron <fladron@gmail.com>
2016-08-05 15:46:53 +0200

    added a button to collapse and uncollapse filter tags

--- sites/all/modules/guanyem/pocket_api/src/app/components/pocket/tagfilterslist.directive.js
+++ sites/all/modules/guanyem/pocket_api/src/app/components/pocket/tagfilterslist.directive.js
@@ -6,6 +6,17 @@ angular.module('bcomupocket')
         templateUrl: APP_CONFIG.baseHref + '/app/components/pocket/tagfilterslist.directive.html',
         link: function(scope, elem, attr) {
             scope.loadTags();
+
+            var $btn_all = elem[0].querySelector('[data-action="see-all-tags"]');
+            $btn_all = angular.element($btn_all);
+            $btn_all.bind('click', function(e){
+                elem.toggleClass('open');
+                if (elem.hasClass('open')){
+                    $btn_all.text('-');
+                }else{
+                    $btn_all.text('+');
+                }
+            });
         }
     };
 });
