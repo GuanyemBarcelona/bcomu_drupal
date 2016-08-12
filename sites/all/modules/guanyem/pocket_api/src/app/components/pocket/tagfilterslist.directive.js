2e02b6fc6c6fcef6d85c195e84855365d2d020e5
Felip Ladron <fladron@gmail.com>
2016-08-12 15:52:54 +0200

    More text for the All tags collapse btn, for the more and the less versions

--- sites/all/modules/guanyem/pocket_api/src/app/components/pocket/tagfilterslist.directive.js
+++ sites/all/modules/guanyem/pocket_api/src/app/components/pocket/tagfilterslist.directive.js
@@ -9,12 +9,13 @@ angular.module('bcomupocket')
 
             var $btn_all = elem[0].querySelector('[data-action="see-all-tags"]');
             $btn_all = angular.element($btn_all);
+            $btn_all.text($btn_all.attr('data-text-more'));
             $btn_all.bind('click', function(e){
                 elem.toggleClass('open');
                 if (elem.hasClass('open')){
-                    $btn_all.text('-');
+                    $btn_all.text($btn_all.attr('data-text-less'));
                 }else{
-                    $btn_all.text('+');
+                    $btn_all.text($btn_all.attr('data-text-more'));
                 }
             });
         }
