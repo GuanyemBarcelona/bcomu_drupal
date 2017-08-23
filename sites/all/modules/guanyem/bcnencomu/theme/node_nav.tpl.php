ffdc4dd6c37a332a640cfbebe56a10c2dd1c8924
Felip Ladron <fladron@notsoda.com>
2017-08-23 13:45:07 +0200

    moved custom modules and theme to custom folders

--- sites/all/modules/guanyem/bcnencomu/theme/node_nav.tpl.php
+++
@@ -1,7 +0,0 @@
-<nav class="node-navigation">
-  <ul>
-    <?php foreach ($links as $key => $link) { ?>
-    <li data-dir="<?php print $key; ?>"><?php print $link; ?></li>
-    <?php } ?>
-  </ul>
-</nav>
--- sites/all/modules/guanyem/bcnencomu/theme/social_network_links.tpl.php
+++
@@ -1,9 +0,0 @@
-<div class="social-network-list">
-  <ul>
-  <?php foreach ($list as $item){ ?>
-    <li>
-    	<a href="<?php print $item['url']; ?>" title="<?php print $item['name']; ?>" data-social-network="<?php print $item['machine_name']; ?>" rel="external"><i class="fa <?php print $item['fa_name']; ?>"></i> <span><?php print $item['name']; ?></span></a>
-    </li>
-  <?php } ?>
-  </ul>
-</div>
