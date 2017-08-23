2125d064f7efc3055eea04c2cc4ba84bbe2ce0f7
Felip Ladron <fladron@notsoda.com>
2017-08-23 19:44:24 +0200

    fixing module's theme share methods

--- sites/all/modules/custom/guanyem/bcnencomu/theme/social_network_links.tpl.php
+++ sites/all/modules/custom/guanyem/bcnencomu/theme/social_network_links.tpl.php
@@ -1,9 +1,9 @@
 <div class="social-network-list">
-  <ul>
-  <?php foreach ($list as $item){ ?>
-    <li>
-    	<a href="<?php print $item['url']; ?>" title="<?php print $item['name']; ?>" data-social-network="<?php print $item['machine_name']; ?>" rel="external"><i class="fa <?php print $item['fa_name']; ?>"></i> <span><?php print $item['name']; ?></span></a>
-    </li>
-  <?php } ?>
-  </ul>
+    <ul>
+        <?php foreach ($list as $item){ ?>
+            <li>
+                <a href="<?php print $item['url']; ?>" title="<?php print $item['name']; ?>" data-icon data-social-network="<?php print $item['machine_name']; ?>" rel="<?php print $item['rel']; ?>"><span><?php print $item['name']; ?></span></a>
+            </li>
+        <?php } ?>
+    </ul>
 </div>
