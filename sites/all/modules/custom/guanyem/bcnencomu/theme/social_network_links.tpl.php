21872bd996b3f01c37b94e3f7ca7ff6ffc1b61e5
Felip Ladron <fladron@notsoda.com>
2017-08-24 18:58:59 +0200

    tpl changes to share links tpls

--- sites/all/modules/custom/guanyem/bcnencomu/theme/social_network_links.tpl.php
+++ sites/all/modules/custom/guanyem/bcnencomu/theme/social_network_links.tpl.php
@@ -2,7 +2,7 @@
     <ul>
         <?php foreach ($list as $item){ ?>
             <li>
-                <a href="<?php print $item['url']; ?>" title="<?php print $item['name']; ?>" data-icon data-social-network="<?php print $item['machine_name']; ?>" rel="<?php print $item['rel']; ?>"><span><?php print $item['name']; ?></span></a>
+                <a href="<?php print $item['url']; ?>" title="<?php print $item['name']; ?>" data-icon data-social-network="<?php print $item['machine_name']; ?>" rel="<?php print $item['rel']; ?>"><span class="label"><?php print $item['name']; ?></span></a>
             </li>
         <?php } ?>
     </ul>
