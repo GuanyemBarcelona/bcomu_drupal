896000100ce120c14033f39b9d0cecfed81d5a9d
Felip <felip@MacBook-Pro-de-Felip-2.local>
2017-08-28 15:56:26 +0200

    tpl changes to Espai detail

--- sites/all/themes/custom/bcnencomu/templates/node--eix_tematic.tpl.php
+++ sites/all/themes/custom/bcnencomu/templates/node--eix_tematic.tpl.php
@@ -6,49 +6,16 @@
   <?php } else if ($view_mode == 'full') { ?>
   <?php /* ----------------- FULL DISPLAY ----------------- */ ?>
   <header>
-    <?php if (isset($share_links)){ ?>
-      <?php print $share_links; ?>
-    <?php } ?>
     <?php if (isset($title)) { ?>
     <h1><?php print $title; ?></h1>
     <?php } ?>
-    <?php print $back_link; ?>
   </header>
   <div class="content">
     <?php 
       hide($content['comments']);
       hide($content['links']);
-      hide($content['field_grup_email']);
-      hide($content['field_grup_web']);
-      hide($content['field_grup_facebook']);
-      hide($content['field_grup_twitter']);
       print render($content);
      ?>
-
-    <aside class="extra-links">
-      <?php print render($content['field_grup_email']); ?>
-      <?php if (isset($web_link)){ ?>
-      <div class="field field-name-field-grup-web">
-        <div class="field-item">
-          <?php print $web_link; ?>
-        </div>
-      </div>
-      <?php } ?>
-      <?php if (isset($facebook_link)){ ?>
-      <div class="field field-name-field-grup-facebook">
-        <div class="field-item">
-          <?php print $facebook_link; ?>
-        </div>
-      </div>
-      <?php } ?>
-      <?php if (isset($twitter_link)){ ?>
-      <div class="field field-name-field-grup-twitter">
-        <div class="field-item">
-          <?php print $twitter_link; ?>
-        </div>
-      </div>
-      <?php } ?>
-    </aside>
   </div>
   <?php } ?>
 </<?php print $tag; ?>> <!-- /node-->
