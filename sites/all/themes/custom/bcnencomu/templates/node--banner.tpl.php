232a41c850fa546d692c1b3c34a988c1f8e7b1e9
Felip Ladrón <fladron@notsoda.com>
2018-01-23 15:58:48 +1100

    banner teaser tpl: wrapped html inside .content div; display link url, not display_url

--- sites/all/themes/custom/bcnencomu/templates/node--banner.tpl.php
+++ sites/all/themes/custom/bcnencomu/templates/node--banner.tpl.php
@@ -2,22 +2,24 @@
 <<?php print $tag; ?> id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>">
   <?php if ($view_mode == 'teaser' || $view_mode == 'slider') { ?>
   <?php /* ----------------- TEASER DISPLAY ----------------- */ ?>
-  <?php if (isset($teaser_image)){ ?>
-  <div class="image">
-    <?php if (empty($content['field_link'])){ ?>
-    <?php print $teaser_image; ?> <?php // if does not have a link, print just the image ?>
-    <?php }else{ ?>               <?php // else ?>
-    <?php if (!$hide_button){ ?>  <?php //   if we must show a button, print the image and the button ?>
-    <?php print $teaser_image; ?> 
-    <?php print render($content['field_link']); ?>
-    <?php }else{ ?>               <?php //   else, print a linked image ?>
-    <a href="<?php print $content['field_link']['#items'][0]['display_url']; ?>"<?php if ($external_link){ ?> rel="external"<?php } ?>>
+  <div class="content">
+    <?php if (isset($teaser_image)){ ?>
+    <div class="image">
+      <?php if (empty($content['field_link'])){ ?>
+      <?php print $teaser_image; ?> <?php // if does not have a link, print just the image ?>
+      <?php }else{ ?>               <?php // else ?>
+      <?php if (!$hide_button){ ?>  <?php //   if we must show a button, print the image and the button ?>
       <?php print $teaser_image; ?>
-    </a>
-    <?php } ?>
+      <?php print render($content['field_link']); ?>
+      <?php }else{ ?>               <?php //   else, print a linked image ?>
+      <a href="<?php print $content['field_link']['#items'][0]['url']; ?>"<?php if ($external_link){ ?> rel="external"<?php } ?>>
+        <?php print $teaser_image; ?>
+      </a>
+      <?php } ?>
+      <?php } ?>
+    </div>
     <?php } ?>
   </div>
-  <?php } ?>
   <?php } else if ($view_mode == 'highlighted') { ?>
 
     <?php if (isset($content['field_link'])){ ?>
