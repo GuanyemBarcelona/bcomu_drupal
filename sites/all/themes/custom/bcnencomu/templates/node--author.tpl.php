0bcd049cb862d5b249d495aff1c9b5ea537edfbf
Felip Ladrón <fladron@notsoda.com>
2018-09-11 17:15:17 +0200

    tpl for Author

---
+++ sites/all/themes/custom/bcnencomu/templates/node--author.tpl.php
@@ -0,0 +1,24 @@
+<?php $tag = ($view_mode != 'full')? 'div' : 'article'; ?>
+<<?php print $tag; ?> id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>">
+  <?php if ($view_mode == 'teaser' || $view_mode == 'highlighted') { ?>
+  <?php /* ----------------- TEASER DISPLAY ----------------- */ ?>
+      <div class="content">
+        <a href="<?php print $node_url; ?>">
+          <?php print render($content['field_image']); ?>
+
+          <h2><?php print $title; ?></h2>
+
+          <?php print render($content['body']); ?>
+        </a>
+      </div>
+  <?php } else if ($view_mode == 'full') { ?>
+  <?php /* ----------------- FULL DISPLAY ----------------- */ ?>
+  <div class="content">
+    <?php print render($content['field_image']); ?>
+
+    <h1><?php print $title; ?></h1>
+
+    <?php print render($content['body']); ?>
+  </div>
+  <?php } ?>
+</<?php print $tag; ?>> <!-- /node-->
