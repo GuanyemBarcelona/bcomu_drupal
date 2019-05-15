81a9fe69295318a33fd73c7527ff7be64d951717
Felip Ladrón <fladron@notsoda.com>
2019-05-15 16:34:14 +0200

    updated gulp to v4

--- sites/all/themes/custom/bcnencomu/gulpfile.js
+++ sites/all/themes/custom/bcnencomu/gulpfile.js
@@ -1,23 +1,25 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
+"use strict";
 
-// styles
-gulp.task('styles', function() {
-  gulp.src('sass/styles.scss')
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+
+
+function styles() {
+  return gulp
+    .src('sass/styles.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('css'));
-});
+}
 
-//watch
-gulp.task('watch', function() {
-  //watch .scss files
-  gulp.watch('sass/**/*.scss', ['styles']);
-});
+function watch() {
+  gulp.watch('sass/**/*.scss', styles);
+}
 
-// The default task
-gulp.task('default', ['styles']);
+exports.styles = styles;
+exports.watch = watch;
+exports.default = watch;
