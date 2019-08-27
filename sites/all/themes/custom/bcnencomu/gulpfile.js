751c0cb574850e3b3917a11bd4323fcb519231a6
Robin Jiménez Newman <robin@notsoda.com>
2019-08-27 14:38:32 +0200

    Gulpfile changes

--- sites/all/themes/custom/bcnencomu/gulpfile.js
+++ sites/all/themes/custom/bcnencomu/gulpfile.js
@@ -1,25 +1,34 @@
-"use strict";
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssnano = require("cssnano"),
+    sourcemaps = require("gulp-sourcemaps");
 
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
+var paths = {
+    styles: {
+        src: "sass/**/*.scss",
+        dest: "css"
+    }
+};
 
-
-function styles() {
-  return gulp
-    .src('sass/styles.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('css'));
+function style() {
+    return (
+        gulp
+            .src(paths.styles.src)
+            .pipe(sourcemaps.init())
+            .pipe(sass())
+            .on("error", sass.logError)
+            .pipe(postcss([autoprefixer(), cssnano()]))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(paths.styles.dest))
+    );
 }
 
-function watch() {
-  gulp.watch('sass/**/*.scss', styles);
+function watch(){
+    gulp.watch(paths.styles.src, style)
 }
 
-exports.styles = styles;
+exports.style = style;
 exports.watch = watch;
-exports.default = watch;
+exports.default = style;
