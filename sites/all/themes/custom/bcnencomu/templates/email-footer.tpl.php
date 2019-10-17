4f9108e6b3f1523e24bcd0e9ca257d5d5b9fbbcc
Felip Ladrón <fladron@notsoda.com>
2019-10-17 11:42:32 +0200

    templating for the emails

---
+++ sites/all/themes/custom/bcnencomu/templates/email-footer.tpl.php
@@ -0,0 +1,121 @@
+            </td>
+          </tr>
+        </tbody></table>
+      </div>
+      <!--[if (gte mso 9)|(lte ie 8)]></td></tr></table><![endif]-->
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+<table class="vb-outer" width="100%" cellpadding="0" border="0" cellspacing="0" bgcolor="#ffffff" style="background-color: #ffffff;" id="ko_tripleImageBlock_1">
+  <tbody><tr>
+    <td class="vb-outer" valign="top" align="center" style="padding-left: 9px; padding-right: 9px;">
+      <!--[if (gte mso 9)|(lte ie 8)]><table align="center" border="0" cellspacing="0" cellpadding="0" width="570"><tr><td align="center" valign="top"><![endif]-->
+      <div class="oldwebkit" style="max-width: 570px;">
+        <table width="570" class="vb-container fullwidth" cellpadding="0" border="0" bgcolor="#ffffff" align="center" cellspacing="0" style="border-collapse: separate; padding-left: 0; padding-right: 0; width: 100%; max-width: 570px; background-color: #ffffff;">
+          <tbody><tr>
+            <td valign="top" align="center" class="mobile-row" style="font-size: 0;">
+
+              <!--[if (gte mso 9)|(lte ie 8)]><table align="center" border="0" cellspacing="0" cellpadding="0" width="570"><tr><![endif]-->
+              <!--[if (gte mso 9)|(lte ie 8)]><td align="left" valign="top" width="190"><![endif]-->
+              <div style="display: inline-block; max-width: 190px; vertical-align: top; width: 100%;" class="mobile-full">
+                <a target="_new" href="https://barcelonaencomu.cat" style="color: #3f3f3f; color: #3f3f3f; text-decoration: none;"><img hspace="0" align="left" border="0" vspace="0" width="190" height="80" class="mobile-full" alt="" style="border: 0px; display: block; border-radius: 0px; width: 100%; height: auto; font-size: 15px; font-family: Arial, Helvetica, sans-serif; color: #000000;" src="<?php echo $images_url ?>logo_bcomu.png"></a>
+              </div><!--[if (gte mso 9)|(lte ie 8)]></td>
+              <td align="left" valign="top" width="190">
+              <![endif]--><div style="display: inline-block; max-width: 190px; vertical-align: top; width: 100%;" class="mobile-full">
+                <a target="_new" href="https://barcelonaencomu.cat" style="color: #3f3f3f; color: #3f3f3f; text-decoration: none;"><img hspace="0" align="left" border="0" vspace="0" width="190" height="80" class="mobile-full" alt="" style="border: 0px; display: block; border-radius: 0px; width: 100%; height: auto; font-size: 15px; font-family: Arial, Helvetica, sans-serif; color: #000000;" src="<?php echo $images_url ?>visitans.png"></a>
+              </div><!--[if (gte mso 9)|(lte ie 8)]></td>
+              <td align="left" valign="top" width="190">
+              <![endif]--><div style="display: inline-block; max-width: 190px; vertical-align: top; width: 100%;" class="mobile-full">
+                <a target="_new" href="https://api.whatsapp.com/send?phone=34622665234&amp;text=Afegeix-me%20al%20Whatsapp%20de%20Barcelona%20en%20Com%c3%ba&amp;source=&amp;data=" style="color: #3f3f3f; color: #3f3f3f; text-decoration: none;"><img hspace="0" align="right" border="0" vspace="0" width="190" height="80" class="mobile-full" alt="" style="border: 0px; display: block; border-radius: 0px; width: 100%; height: auto; font-size: 15px; font-family: Arial, Helvetica, sans-serif; color: #000000;" src="<?php echo $images_url ?>whatsapp.png"></a>
+              </div>
+              <!--[if (gte mso 9)|(lte ie 8)]></td><![endif]-->
+              <!--[if (gte mso 9)|(lte ie 8)]></tr></table><![endif]-->
+            </td>
+          </tr>
+          </tbody></table>
+
+      </div>
+      <!--[if (gte mso 9)|(lte ie 8)]></td></tr></table><![endif]-->
+    </td>
+  </tr>
+  </tbody>
+</table>
+
+<table width="100%" cellpadding="0" border="0" cellspacing="0" bgcolor="#ffffff" style="background-color: #ffffff;" id="ko_socialBlock_2">
+  <tbody><tr>
+    <td align="center" valign="top" bgcolor="#ffffff" style="background-color: #ffffff;">
+      <!--[if (gte mso 9)|(lte ie 8)]><table align="center" border="0" cellspacing="0" cellpadding="0" width="570"><tr><td align="center" valign="top"><![endif]-->
+      <div class="oldwebkit" style="max-width: 570px;">
+        <table width="570" style="border-collapse: separate; border-spacing: 9px; width: 100%; max-width: 570px;" border="0" cellpadding="0" cellspacing="9" class="vb-row fullpad" align="center">
+          <tbody><tr>
+            <td valign="top" align="center" style="font-size: 0;">
+
+              <!--[if (gte mso 9)|(lte ie 8)]><table align="center" border="0" cellspacing="0" cellpadding="0" width="552"><tr><![endif]-->
+              <!--[if (gte mso 9)|(lte ie 8)]><td align="left" valign="top" width="276"><![endif]-->
+              <div style="display: inline-block; max-width: 276px; vertical-align: top; width: 100%;" class="mobile-full">
+
+                <table class="vb-content" border="0" cellspacing="9" cellpadding="0" width="276" style="border-collapse: separate; width: 100%;" align="left">
+                  <tbody><tr>
+                    <td valign="middle" align="left" class="long-text links-color mobile-textcenter" style="font-size: 13px; font-family: Arial, Helvetica, sans-serif; color: #919191; text-align: left;"><p style="margin: 1em 0px; margin-bottom: 0px; margin-top: 0px;"><br data-mce-bogus="1"></p></td>
+                  </tr>
+                  </tbody></table>
+
+              </div><!--[if (gte mso 9)|(lte ie 8)]></td>
+              <td align="left" valign="top" width="276">
+              <![endif]--><div style="display: inline-block; max-width: 276px; vertical-align: top; width: 100%;" class="mobile-full">
+
+                <table class="vb-content" border="0" cellspacing="9" cellpadding="0" width="276" style="border-collapse: separate; width: 100%;" align="right">
+                  <tbody><tr>
+
+                    <td align="right" valign="middle" class="links-color socialLinks mobile-textcenter" style="font-size: 6px;">
+                      &nbsp;
+                      <a target="_new" href="https://www.facebook.com/bcnencomu" style="display: inline-block; color: #cccccc; color: #cccccc; text-decoration: underline;">
+                        <img src="<?php echo $images_url ?>facebook_bw.png" alt="Facebook" border="0" class="socialIcon" style="border: 0px; display: inline-block; vertical-align: top; padding-bottom: 0px; border-radius: 100%;">
+                      </a>
+                      &nbsp;
+                      <a target="_new" href="https://twitter.com/bcnencomu" style="display: inline-block; color: #cccccc; color: #cccccc; text-decoration: underline;">
+                        <img src="<?php echo $images_url ?>twitter_bw.png" alt="Twitter" border="0" class="socialIcon" style="border: 0px; display: inline-block; vertical-align: top; padding-bottom: 0px; border-radius: 100%;">
+                      </a>
+
+                      <a target="_new" href="https://www.instagram.com/barcelonaencomu/" style="display: inline-block; color: #cccccc; color: #cccccc; text-decoration: underline;">
+                        <img src="<?php echo $images_url ?>instagram_bw.png" alt="Instagram" border="0" class="socialIcon" style="border: 0px; display: inline-block; vertical-align: top; padding-bottom: 0px; border-radius: 100%;">
+                      </a>
+                    </td>
+                  </tr>
+                  </tbody></table>
+
+              </div>
+              <!--[if (gte mso 9)|(lte ie 8)]></td><![endif]-->
+              <!--[if (gte mso 9)|(lte ie 8)]></tr></table><![endif]-->
+
+            </td>
+          </tr>
+          </tbody></table>
+      </div>
+      <!--[if (gte mso 9)|(lte ie 8)]></td></tr></table><![endif]-->
+    </td>
+  </tr>
+  </tbody>
+</table>
+
+<table class="vb-outer" width="100%" cellpadding="0" border="0" cellspacing="0" bgcolor="#ffffff" style="background-color: #ffffff;" id="ko_textBlock_5">
+  <tbody><tr>
+    <td class="vb-outer" align="center" valign="top" bgcolor="#ffffff" style="padding-left: 9px; padding-right: 9px; background-color: #ffffff;">
+
+      <!--[if (gte mso 9)|(lte ie 8)]><table align="center" border="0" cellspacing="0" cellpadding="0" width="570"><tr><td align="center" valign="top"><![endif]-->
+      <div class="oldwebkit" style="max-width: 570px;">
+        <table width="570" border="0" cellpadding="0" cellspacing="18" class="vb-container fullpad" bgcolor="#ffffff" style="border-collapse: separate; border-spacing: 18px; padding-left: 0; padding-right: 0; width: 100%; max-width: 570px; background-color: #ffffff;">
+          <tbody><tr>
+            <td align="left" class="long-text links-color" style="text-align: left; font-size: 15px; font-family: Arial, Helvetica, sans-serif; color: #000000;"><h5><span style="color: rgb(128, 128, 128);" data-mce-style="color: #808080;">Aquest email ha estat enviat a <?php echo $to ?> perquè t'has inscrit a un dels formularis del web de Barcelona En Comú.&nbsp;<br><br></span><span style="color: rgb(128, 128, 128);" data-mce-style="color: #808080;">Les vostres dades formen part d'un fitxer automatitzat titularitat de BARCELONA EN COMÚ, notificat a l'Agència Espanyola de Protecció de Dades. Podeu exercir els drets d'accés, rectificació, limitació al tractament, supressió, portabilitat i oposició a: C/ Marina 131 bis, baixos (08013 - Barcelona) o al correu&nbsp;lopd@bcnencomu.cat</span></h5></td>
+          </tr>
+          </tbody></table>
+      </div>
+      <!--[if (gte mso 9)|(lte ie 8)]></td></tr></table><![endif]-->
+    </td>
+  </tr>
+  </tbody>
+</table>
+</body>
+</html>
---
+++ sites/all/themes/custom/bcnencomu/templates/email-header.tpl.php
@@ -0,0 +1,145 @@
+  <style type="text/css">
+    body{ margin: 0; padding: 0; }
+    img{ border: 0px; display: block; }
+
+    .socialLinks{ font-size: 6px; }
+    .socialLinks a{
+      display: inline-block;
+    }
+    .socialIcon{
+      display: inline-block;
+      vertical-align: top;
+      padding-bottom: 0px;
+      border-radius: 100%;
+    }
+    .oldwebkit{ max-width: 570px; }
+    td.vb-outer{ padding-left: 9px; padding-right: 9px; }
+    table.vb-container, table.vb-row, table.vb-content{
+      border-collapse: separate;
+    }
+    table.vb-row{
+      border-spacing: 9px;
+    }
+    table.vb-row.halfpad{
+      border-spacing: 0;
+      padding-left: 9px;
+      padding-right: 9px;
+    }
+    table.vb-row.fullwidth{
+      border-spacing: 0;
+      padding: 0;
+    }
+    table.vb-container{
+      padding-left: 18px;
+      padding-right: 18px;
+    }
+    table.vb-container.fullpad{
+      border-spacing: 18px;
+      padding-left: 0;
+      padding-right: 0;
+    }
+    table.vb-container.halfpad{
+      border-spacing: 9px;
+      padding-left: 9px;
+      padding-right: 9px;
+    }
+    table.vb-container.fullwidth{
+      padding-left: 0;
+      padding-right: 0;
+    }
+  </style>
+  <style type="text/css">
+    /* yahoo, hotmail */
+    .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div{ line-height: 100%; }
+    .yshortcuts a{ border-bottom: none !important; }
+    .vb-outer{ min-width: 0 !important; }
+    .RMsgBdy, .ExternalClass{
+      width: 100%;
+      background-color: #ffffff}
+
+    /* outlook */
+    table{ mso-table-rspace: 0pt; mso-table-lspace: 0pt; }
+    #outlook a{ padding: 0; }
+    img{ outline: none; text-decoration: none; border: none; -ms-interpolation-mode: bicubic; }
+    a img{ border: none; }
+
+    @media screen and (max-device-width: 600px), screen and (max-width: 600px) {
+      table.vb-container, table.vb-row{
+        width: 95% !important;
+      }
+
+      .mobile-hide{ display: none !important; }
+      .mobile-textcenter{ text-align: center !important; }
+
+      .mobile-full{
+        float: none !important;
+        width: 100% !important;
+        max-width: none !important;
+        padding-right: 0 !important;
+        padding-left: 0 !important;
+      }
+      img.mobile-full{
+        width: 100% !important;
+        max-width: none !important;
+        height: auto !important;
+      }   
+    }
+  </style>
+  <style type="text/css">
+    
+    #ko_textBlock_3 .links-color a, #ko_textBlock_3 .links-color a:link, #ko_textBlock_3 .links-color a:visited, #ko_textBlock_3 .links-color a:hover{
+      color: #3f3f3f;
+      text-decoration: underline
+    }
+    
+    #ko_textBlock_5 .links-color a, #ko_textBlock_5 .links-color a:link, #ko_textBlock_5 .links-color a:visited, #ko_textBlock_5 .links-color a:hover{
+      color: #3f3f3f;
+      text-decoration: underline
+    }
+     #ko_textBlock_3 .long-text p{ margin: 1em 0px }  #ko_textBlock_5 .long-text p{ margin: 1em 0px }  #ko_textBlock_3 .long-text p:last-child{ margin-bottom: 0px }  #ko_textBlock_5 .long-text p:last-child{ margin-bottom: 0px }  #ko_textBlock_3 .long-text p:first-child{ margin-top: 0px }  #ko_textBlock_5 .long-text p:first-child{ margin-top: 0px } 
+    #ko_socialBlock_2 .links-color a, #ko_socialBlock_2 .links-color a:link, #ko_socialBlock_2 .links-color a:visited, #ko_socialBlock_2 .links-color a:hover{
+      color: #cccccc;
+      text-decoration: underline
+    }
+     #ko_socialBlock_2 .long-text p{ margin: 1em 0px }  #ko_socialBlock_2 .long-text p:last-child{ margin-bottom: 0px }  #ko_socialBlock_2 .long-text p:first-child{ margin-top: 0px } 
+    #ko_tripleImageBlock_1 a, #ko_tripleImageBlock_1 a:link, #ko_tripleImageBlock_1 a:visited, #ko_tripleImageBlock_1 a:hover{
+      color: #3f3f3f;
+      text-decoration: underline
+    }
+    
+    #ko_imageBlock_3 a, #ko_imageBlock_3 a:link, #ko_imageBlock_3 a:visited, #ko_imageBlock_3 a:hover{
+      color: #3f3f3f;
+      text-decoration: underline
+    }
+  </style>
+
+  <table class="vb-outer" width="100%" cellpadding="0" border="0" cellspacing="0" bgcolor="#ffffff" style="background-color: #ffffff;" id="ko_imageBlock_3">
+    <tbody><tr>
+      <td class="vb-outer" valign="top" align="center" style="padding-left: 9px; padding-right: 9px;">
+
+<!--[if (gte mso 9)|(lte ie 8)]><table align="center" border="0" cellspacing="0" cellpadding="0" width="570"><tr><td align="center" valign="top"><![endif]-->
+        <div class="oldwebkit" style="max-width: 570px;">
+        <table width="570" class="vb-container fullwidth" cellpadding="0" border="0" bgcolor="#ffffff" align="center" cellspacing="0" style="border-collapse: separate; padding-left: 0; padding-right: 0; width: 100%; max-width: 570px; background-color: #ffffff;">
+          <tbody><tr>
+            <td valign="top" align="center">
+              <img hspace="0" border="0" vspace="0" width="570" class="mobile-full" alt="" style="border: 0px; max-width: 570px; display: block; border-radius: 0px; width: 100%; height: auto; font-size: 15px; font-family: Arial, Helvetica, sans-serif; color: #000000;" src="<?php echo $images_url ?>header.png">
+            </td>
+          </tr>
+        </tbody></table>
+        
+        </div>
+<!--[if (gte mso 9)|(lte ie 8)]></td></tr></table><![endif]-->
+      </td>
+    </tr>
+  </tbody>
+  </table>
+
+  <table class="vb-outer" width="100%" cellpadding="0" border="0" cellspacing="0" bgcolor="#ffffff" style="background-color: #ffffff;" id="ko_textBlock_3">
+    <tbody><tr>
+      <td class="vb-outer" align="center" valign="top" bgcolor="#ffffff" style="padding-left: 9px; padding-right: 9px; background-color: #ffffff;">
+
+<!--[if (gte mso 9)|(lte ie 8)]><table align="center" border="0" cellspacing="0" cellpadding="0" width="570"><tr><td align="center" valign="top"><![endif]-->
+        <div class="oldwebkit" style="max-width: 570px;">
+        <table width="570" border="0" cellpadding="0" cellspacing="18" class="vb-container fullpad" bgcolor="#ffffff" style="border-collapse: separate; border-spacing: 18px; padding-left: 0; padding-right: 0; width: 100%; max-width: 570px; background-color: #ffffff;">
+          <tbody><tr>
+            <td align="left" class="long-text links-color" style="text-align: left; font-size: 15px; font-family: Arial, Helvetica, sans-serif; color: #000000;">
