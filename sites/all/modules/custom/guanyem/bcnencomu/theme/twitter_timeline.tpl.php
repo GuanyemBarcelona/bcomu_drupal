d1e24dfdab948ae42fd944e2229379cb6f0bb319
Felip Ladrón <fladron@notsoda.com>
2020-03-10 16:20:55 +0100

    added a simple twitter embed code

---
+++ sites/all/modules/custom/guanyem/bcnencomu/theme/twitter_timeline.tpl.php
@@ -0,0 +1,1 @@
+<a class="twitter-timeline" data-width="300" data-height="600" data-theme="dark" href="https://twitter.com/bcnencomu?ref_src=twsrc%5Etfw">Tweets by bcnencomu</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
