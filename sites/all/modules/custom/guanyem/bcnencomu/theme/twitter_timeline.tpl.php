21ba8ef80fc1a5d3bd36b57701c36ca09bb5b494
Felip Ladrón <fladron@notsoda.com>
2020-03-12 10:25:42 +0100

    template for the Twitter timeline

--- sites/all/modules/custom/guanyem/bcnencomu/theme/twitter_timeline.tpl.php
+++ sites/all/modules/custom/guanyem/bcnencomu/theme/twitter_timeline.tpl.php
@@ -1,1 +1,5 @@
-<a class="twitter-timeline" data-width="300" data-height="600" data-theme="dark" href="https://twitter.com/bcnencomu?ref_src=twsrc%5Etfw">Tweets by bcnencomu</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+<a class="twitter-timeline"
+   data-width="300" data-height="600"
+   data-theme="dark"
+   data-chrome="noheader"
+   href="https://twitter.com/bcnencomu?ref_src=twsrc%5Etfw">Tweets by bcnencomu</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
